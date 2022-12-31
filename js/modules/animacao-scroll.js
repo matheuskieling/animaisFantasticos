ef398f0331e8cbd1c5a0151837e70b17173deb65
Matheus Kieling <matheus.kieling@hotmail.com>
2022-12-30 21:04:11 -0300

    refatoracao com class do scroll-anima

--- js/modules/animacao-scroll.js
+++ js/modules/animacao-scroll.js
@@ -1,10 +1,14 @@
-export default function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('[data-anime="scroll"]');
-  function animaScroll() {
-    sections.forEach((section) => {
-      const windowMetade = window.innerHeight * 0.6;
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - windowMetade < 0;
+      const isSectionVisible = (sectionTop - this.windowMetade) < 0;
       if (isSectionVisible) {
         section.classList.add('ativo');
       } else if (section.classList.contains('ativo')) {
@@ -12,8 +16,10 @@ export default function initAnimacaoScroll() {
       }
     });
   }
-  if (sections.length) {
-    animaScroll();
-    window.addEventListener('scroll', animaScroll);
+
+  init() {
+    this.animaScroll();
+    window.addEventListener('scroll', this.animaScroll);
+    return this;
   }
 }
